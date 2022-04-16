505e4531c214530987a0ae9b63bb941ff2868bcf
rolanusonov <rolanusonov031209@gmail.com>
2022-04-16 14:14:33 +0300

    inti

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Faq = () => {
     return (
         <div>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, quisquam.</h1>
+
+
         </div>
     );
 };
--- src/pages/Woman.js
+++ src/pages/Woman.js
@@ -67,15 +67,15 @@ const Woman = ({el}) => {
                                 </button>
 
 
-                                {/*                    <button*/}
+                                                    <button
 
-                                {/*                        onClick={() => dispatch({type: "ADD_TO_FAVORITE", payload: el})}*/}
-                                {/*                        className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-800">*/}
-                                {/*<span*/}
-                                {/*    className="relative px-5 py-2.5 transition-all  duration-30 bg-white dark:bg-gray-600 rounded-md group-hover:bg-opacity-0">*/}
-                                {/*     <FaBeer/>*/}
-                                {/*</span>*/}
-                                {/*                    </button>*/}
+                                                        onClick={() => dispatch({type: "ADD_TO_FAVORITE", payload: el})}
+                                                        className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-800">
+                                <span
+                                    className="relative px-5 py-2.5 transition-all  duration-30 bg-white dark:bg-gray-600 rounded-md group-hover:bg-opacity-0">
+                                     <FaBeer/>
+                                </span>
+                                                    </button>
 
 
                             </div>
