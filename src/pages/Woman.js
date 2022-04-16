1dcdfdda8b5dd40fa1149e70bf0fcb92c45fe5f6
rolanusonov <rolanusonov031209@gmail.com>
2022-04-16 18:37:25 +0300

    fdj

--- src/pages/Woman.js
+++ src/pages/Woman.js
@@ -40,10 +40,10 @@ const Woman = ({el}) => {
 
 
 
-                    <div className=" w-72  ">
+                    <div className=" w-72 Link ">
 
 
-                        <div className="">
+                        <div className=" ">
                             <img src={el.img} alt="wer"/>
                             <h1 className="text-lg text-black">{el.description}</h1>
                             <h1 className="text-lg text-black absolute mx-16 mt-4">{el.discounts} </h1>
