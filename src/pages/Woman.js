03ff115cdbf4e6c88c104baa7e72eb3d90820740
rolanusonov <rolanusonov031209@gmail.com>
2022-04-16 13:09:46 +0300

    init

--- src/pages/Woman.js
+++ src/pages/Woman.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
 // import {getShop} from "../Redux/actoion";
 import {FaBeer} from 'react-icons/fa';
@@ -32,10 +32,31 @@ const Woman = ({el}) => {
 
     return (
         <div className="handle flex items-center justify-center ">
+            <div>
+                <Link to={`/categories/${el.id}`} className=" ">
 
+                    < img className="p-2 img dark:bg-gray-500 z-1" style={{width:"607px", height:" 252px"}} src={el.img} alt="product image!!!"/>
+
+
+
+                    <Link to="/" className="svg">
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white title">{el.title}</h5>
+
+                        <svg width="25" height="16" viewBox="0 0 25 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="path">
+                            <path d="M24.7071 8.70711C25.0976 8.31658 25.0976 7.68342 24.7071 7.2929L18.3431 0.928934C17.9526 0.538409 17.3195 0.538409 16.9289 0.928934C16.5384 1.31946 16.5384 1.95262 16.9289 2.34315L22.5858 8L16.9289 13.6569C16.5384 14.0474 16.5384 14.6805 16.9289 15.0711C17.3195 15.4616 17.9526 15.4616 18.3431 15.0711L24.7071 8.70711ZM-8.74228e-08 9L24 9L24 7L8.74228e-08 7L-8.74228e-08 9Z" fill="white"/>
+                        </svg>
+
+                    </Link>
+
+                </Link>
+            </div>
 
             {
                 women?.category_detail?.map(el => (
+
+
+
+
                     <div className=" w-72  ">
 
 
@@ -76,7 +97,15 @@ const Woman = ({el}) => {
 
                             </div>
                         </div>
+
+
                     </div>
+
+
+
+
+
+
                 ))
             }
 
